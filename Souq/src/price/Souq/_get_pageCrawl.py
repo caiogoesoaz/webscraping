edbb39e8831b2834a7859a6c84c0c2fbf7155a40
Caio Goes <caiocezar23042001@gmail.com>
2024-11-22 09:52:01 -0300

    teste-3

--- Souq/src/price/Souq/_get_pageCrawl.py
+++ Souq/src/price/Souq/_get_pageCrawl.py
@@ -8,7 +8,7 @@ import pandas as pd
 from unicodedata import normalize
 import re
 from time import sleep
-
+print("ola")
 # Lista de User-Agents
 USER_AGENTS = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
